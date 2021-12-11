f7039791faadbe03831187e9a80f6a16034ef45f
Gaurav_Singh <gauravkarki038@gmail.com>
2021-12-11 15:14:58 +0530

    change1

--- src/components/AboutCount/AboutCount.js
+++ src/components/AboutCount/AboutCount.js
@@ -5,43 +5,43 @@ import { FaApple} from "react-icons/fa";
 function AboutCount() {
     return (
         <div>
-             <section id="counts" class="counts">
-    <div class="container" data-aos="fade-up">
+             <section id="counts" className="counts">
+    <div className="container" data-aos="fade-up">
 
-      <div class="row no-gutters">
+      <div className="row no-gutters">
 
-        <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
-          <div class="count-box">
+        <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+          <div className="count-box">
                <FaApple size={70} />
-            <span data-purecounter-start="0" data-purecounter-end="95" data-purecounter-duration="1" class="purecounter"></span>
+            <span className="purecounter">50</span>
             <p><strong>Doctors</strong> consequuntur quae qui deca rode</p>
             <a href="#">Find out more &raquo;</a>
           </div>
         </div>
 
-        <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
-          <div class="count-box">
+        <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+          <div className="count-box">
             <FaApple size={70} />
-            <span data-purecounter-start="0" data-purecounter-end="36" data-purecounter-duration="1" class="purecounter"></span>
+            <span className="purecounter">50</span>         
             <p><strong>Departments</strong> adipisci atque cum aut numquam delectus</p>
             <a href="#">Find out more &raquo;</a>
           </div>
         </div>
 
-        <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
-          <div class="count-box">
+        <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+          <div className="count-box">
             <FaApple size={70} />
-            <span data-purecounter-start="0" data-purecounter-end="23" data-purecounter-duration="1" class="purecounter"></span>
+            <span className="purecounter">50</span>
             <p><strong>Research Lab</strong> aut commodi quaerat. Aliquam ratione</p>
             <a href="#">Find out more &raquo;</a>
           </div>
         </div>
 
-        <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
-          <div class="count-box">
+        <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+          <div className="count-box">
                 <FaApple size={70} />
-            <span data-purecounter-start="0" data-purecounter-end="250" data-purecounter-duration="1" class="purecounter"></span>
-            <p><strong>Awards</strong> rerum asperiores dolor molestiae doloribu</p>
+                <span className="purecounter">50</span>            
+                <p><strong>Awards</strong> rerum asperiores dolor molestiae doloribu</p>
             <a href="#">Find out more &raquo;</a>
           </div>
         </div>
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -6,39 +6,39 @@ function Services() {
     return (
         <div>
             
-    <section id="featured-services" class="featured-services">
-      <div class="container" data-aos="fade-up">
+    <section id="featured-services" className="featured-services">
+      <div className="container" data-aos="fade-up">
 
-        <div class="row">
-          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
-              <div class="icon"><FaUserMd size={70}/> </div>
-              <h4 class="title"><a href=""> Normal checkup</a></h4>
-              <p class="description">Voluptatum deleniti atque corrupti 2 times per month quas molestias excepturi</p>
+        <div className="row">
+          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div className="icon"><FaUserMd size={70}/> </div>
+              <h4 className="title"><a href=""> Normal checkup</a></h4>
+              <p className="description">Voluptatum deleniti atque corrupti 2 times per month quas molestias excepturi</p>
             </div>
           </div>
 
-          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="400">
-              <div class="icon"><FaUserMd size={70}/>  </div>
-              <h4 class="title"><a href="">Blood Test</a></h4>
-              <p class="description">At vero eos et accusamus et 1 week odio dignissimos ducimus qui blanditiis</p>
+          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="400">
+              <div className="icon"><FaUserMd size={70}/>  </div>
+              <h4 className="title"><a href="">Blood Test</a></h4>
+              <p className="description">At vero eos et accusamus et 1 week odio dignissimos ducimus qui blanditiis</p>
             </div>
           </div>
           
-          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
-              <div class="icon"><FaUserMd size={70}/>  </div>
-              <h4 class="title"><a href="">Body temperature</a></h4>
-              <p class="description">Excepteur sint occaecat 37.6 non proident, 98.6 sunt in culpa qui officia</p>
+          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="300">
+              <div className="icon"><FaUserMd size={70}/>  </div>
+              <h4 className="title"><a href="">Body temperature</a></h4>
+              <p className="description">Excepteur sint occaecat 37.6 non proident, 98.6 sunt in culpa qui officia</p>
             </div>
           </div>
 
-          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
-              <div class="icon"><FaUserMd size={70}/> </div>
-              <h4 class="title"><a href="">Medicine use</a></h4>
-              <p class="description">Duis aute dolor 10-12 in reprehenderit in voluptate 8-12 velit esse cillum dolore</p>
+          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="200">
+              <div className="icon"><FaUserMd size={70}/> </div>
+              <h4 className="title"><a href="">Medicine use</a></h4>
+              <p className="description">Duis aute dolor 10-12 in reprehenderit in voluptate 8-12 velit esse cillum dolore</p>
             </div>
           </div>
 
